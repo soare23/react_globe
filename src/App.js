62db03a4851febb2c68f3f745b29cdb4de907944
Sorin Mihai Toma <toma.mihai.sorin23@gmail.com>
2020-12-22 22:02:28 +0200

    the data is fetched based on user input and sent down to the components

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import './App.css';
+import { useState } from 'react';
+import CountryDetails from './components/CountryDetails';
+import Globe from './components/Globe';
+import BackToTopArrow from './components/BackToTopArrow';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  const [userInput, setUserInput] = useState();
+  const [country, setCountry] = useState();
+  const [markers, setMarkers] = useState({ markers: [] });
+
+  function handleChange(e) {
+    setUserInput(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch(`https://restcountries.eu/rest/v2/name/${userInput}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(data[0]);
+        addNewMarker(data[0]);
+      });
+  }
+
+  function addNewMarker(country) {
+    setMarkers({
+      markers: [
+        {
+          id: country.alpha2Code,
+          name: country.name,
+          coordinates: country.latlng,
+          color: 'red',
+          value: 40,
+        },
+      ],
+    });
+  }
+
+  if (markers.markers.length === 0) {
+    setMarkers({
+      markers: [
+        {
+          coordinates: [0, 0],
+        },
+      ],
+    });
+  }
+
+  return (
+    <div className="App">
+      <div className="main-container">
+        <div className="form-container">
+          <form onSubmit={handleSubmit}>
+            <input
+              className="form-control mr-sm-2"
+              type="search"
+              placeholder="Enter country name or code"
+              aria-label="Search"
+              name="country"
+              value={userInput}
+              onChange={handleChange}
+            />
+            <Button className="btn btn-sm btn-outline-secondary" type="submit">
+              Search
+            </Button>
+          </form>
+        </div>
+        {country ? <CountryDetails country={country}></CountryDetails> : ''}
+        <Globe markers={markers}></Globe>
+        <BackToTopArrow></BackToTopArrow>
+      </div>
+    </div>
+  );
+}
+
+export default App;
